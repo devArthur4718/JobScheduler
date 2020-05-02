78271983523fddca898de0ee4817b542d3cd726b
Arthur Gomes <devarthur4718@gmail.com>
2020-05-02 17:25:14 -0300

    added check for constraints

--- app/src/main/java/com/devarthur4718/jobscheduler/MainActivity.kt
+++ app/src/main/java/com/devarthur4718/jobscheduler/MainActivity.kt
@@ -1,12 +1,63 @@
 package com.devarthur4718.jobscheduler
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.job.JobInfo
+import android.app.job.JobScheduler
+import android.content.ComponentName
+import android.content.Context
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var mScheduler: JobScheduler? = null
+
+    companion object {
+        const val JOB_ID = 0
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setViews()
     }
+
+    private fun setViews() {
+        btnScheduleJob.setOnClickListener {
+            var selectedNetworkOptions = when (rbNetworkOptions.checkedRadioButtonId) {
+                R.id.rbNoNetwork -> JobInfo.NETWORK_TYPE_NONE
+                R.id.rbAnyNetwork -> JobInfo.NETWORK_TYPE_ANY
+                R.id.rbwifiNetwork -> JobInfo.NETWORK_TYPE_UNMETERED
+                else -> JobInfo.NETWORK_TYPE_NONE
+            }
+            mScheduler = getSystemService(Context.JOB_SCHEDULER_SERVICE) as JobScheduler
+            val serviceName = ComponentName(
+                packageName,
+                NotificationJobService::class.java.name
+            )
+
+            when (selectedNetworkOptions) {
+                JobInfo.NETWORK_TYPE_NONE -> it.context.showToast("Please set at least one Constraint")
+                else -> {
+                    val myJobInfo = JobInfo.Builder(JOB_ID, serviceName).apply {
+                        setRequiredNetworkType(selectedNetworkOptions)
+                    }.build()
+                    mScheduler?.schedule(myJobInfo)
+                    it.context.showToast("Job Scheduled, job will run when \n the constraints are met.")
+                }
+            }
+        }
+
+        btnCancel.setOnClickListener {
+            mScheduler?.let {
+                it.cancelAll()
+                mScheduler = null
+                applicationContext.showToast("Jobs cancelled")
+            }
+
+
+        }
+    }
+
+
 }
--- app/src/main/java/com/devarthur4718/jobscheduler/NotificationJobService.kt
+++ app/src/main/java/com/devarthur4718/jobscheduler/NotificationJobService.kt
@@ -1,21 +1,15 @@
 package com.devarthur4718.jobscheduler
 
 import android.annotation.SuppressLint
-import android.app.NotificationChannel
 import android.app.NotificationManager
-import android.app.PendingIntent
 import android.app.job.JobParameters
 import android.app.job.JobService
-import android.content.Context
-import android.content.Intent
-import android.os.Build
 
 class NotificationJobService() : JobService() {
 
     companion object{
         const val PRIMARY_CHANNEL_ID = "primary_notification_channel"
-        const val JOB_NOTIFICATION = "job_notification"
-        const val REQUEST_CODE = 2
+  
     }
 
     lateinit var mNotifyManager : NotificationManager
@@ -39,19 +33,5 @@ class NotificationJobService() : JobService() {
         return false
     }
 
-    fun createNotificationChannel(){
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val name = JOB_NOTIFICATION
-            val descriptionText = getString(R.string.notification)
-            val importance = NotificationManager.IMPORTANCE_DEFAULT
-            val channel = NotificationChannel(PRIMARY_CHANNEL_ID, name, importance).apply {
-                description = descriptionText
-            }
-            // Register the channel with the system
-            val notificationManager: NotificationManager =
-                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-            notificationManager.createNotificationChannel(channel)
-        }
-    }
 }
